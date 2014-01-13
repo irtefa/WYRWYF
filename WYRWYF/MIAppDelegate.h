4c6cca5e7c4c1c21b42add1a29534af31dc27116
Mohd Irtefa <irtefa1@illinois.edu>
2014-01-12 18:04:51 -0600

    Initial Commit

---
+++ WYRWYF/MIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MIAppDelegate.h
+//  WYRWYF
+//
+//  Created by Mohd Irtefa on 1/12/14.
+//  Copyright (c) 2014 Irtefa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WYRWYFTests/WYRWYFTests.m
@@ -0,0 +1,34 @@
+//
+//  WYRWYFTests.m
+//  WYRWYFTests
+//
+//  Created by Mohd Irtefa on 1/12/14.
+//  Copyright (c) 2014 Irtefa. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WYRWYFTests : XCTestCase
+
+@end
+
+@implementation WYRWYFTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
